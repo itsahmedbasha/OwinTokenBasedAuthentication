fadfc679e78de51a614c5ea2e3359f789b2aa115
AhmedBasha.Shaik kjsystems.in <AhmedBasha.Shaik@kjsystems.in>
2019-11-01 18:06:12 +0530

    created basic version of token based authentication

---
+++ TokenAuth/App_Start/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TokenAuth.Providers;
+
+[assembly: OwinStartup(typeof(TokenAuth.Startup))]
+namespace TokenAuth
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+
+           // HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+
+            OAuthAuthorizationServerOptions oauthserveroptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new Microsoft.Owin.PathString("/token"),
+                //AccessTokenExpireTimeSpan = TimeSpan.from(2),
+                Provider = new SimpleAuthorizationProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(oauthserveroptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+
+
+    }
+}
---
+++ TokenAuth/Providers/SimpleAuthorizationProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace TokenAuth.Providers
+{
+    public class SimpleAuthorizationProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            //context.Validated(identity);
+
+            if (context.UserName == "basha@gmail.com" && context.Password == "admin")
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                //identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                //identity.AddClaim(new Claim(ClaimTypes.Name, "basha"));
+                //identity.AddClaim(new Claim("Email", "basha@gmail.com"));
+
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Username and password invalid");
+            }
+
+
+        }
+    }
+}
